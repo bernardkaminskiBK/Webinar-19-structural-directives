e8845e99f82fc5155e10796e6fbbc656f2f94c98
bernardkaminskibk <bernardkaminski.bk@gmail.com>
2022-05-23 17:00:42 +0200

    create example for structural directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar/toolbar.component';
+import { ContentComponent } from './content/content.component';
+import { UnlessDirective } from './directives/unless.directive';
+import {FormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {RoleAdminDirective} from "./directives/roleAdmin.directive";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    ContentComponent,
+    UnlessDirective,
+    RoleAdminDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
